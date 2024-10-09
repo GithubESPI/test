687d6880a90c77a96159c6f2c7f972efdebcf34f
GithubESPI <165779233+GithubESPI@users.noreply.github.com>
2024-10-09 12:34:57 +0200

    Update upload.ts

--- app/api/upload.ts
+++ app/api/upload.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { sessionId, excelUrl, wordUrl } = req.body;
 
   try {
-    const response = await fetch("http://localhost:8000/upload-and-integrate-excel-and-word", {
+    const response = await fetch("https://backbulletin-bceehth0acgwc7gr.canadacentral-01.azurewebsites.net/upload-and-integrate-excel-and-word", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
