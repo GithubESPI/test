a8a9d46b1957e31a4f95e2b931393c7770a38af1
GithubESPI <165779233+GithubESPI@users.noreply.github.com>
2025-01-30 17:22:19 +0100

    Update upload.ts

--- app/api/upload.ts
+++ app/api/upload.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const response = await fetch(
-      "https://backendespi.fly.dev/upload-and-integrate-excel-and-word",
+      "https://bulletins-app.fly.dev//upload-and-integrate-excel-and-word",
       {
         method: "POST",
         headers: {
