385bf276672e45fd4396e4b6cf50a1d29026698d
GithubESPI <a.vespuce@groupe-espi.fr>
2025-03-26 14:14:25 +0100

    pushing new version V8

--- app/configure/form/page.tsx
+++ app/configure/form/page.tsx
@@ -321,7 +321,6 @@ export default function Home() {
         }),
       });
       const data = await response.json();
-      window.open(data.path);
 
       if (!response.ok) {
         throw new Error(data.error || "Erreur lors de la récupération des données");
