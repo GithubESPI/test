7639f68ad0ff43bf7268a699c20dfb17f98428e1
GithubESPI <a.vespuce@groupe-espi.fr>
2025-04-09 10:25:55 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="sticky z-[100] h-20 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-20 items-center justify-between border-b border-zinc-200">
-          <Link href="/" className="flex z-40 font-semibold">
+          <Link href="/home" className="flex z-40 font-semibold">
             <Image
               src="/images/logo.png"
               width={240}
