4435c92befd070ee613885da7812132e2366c76a
GithubESPI <165779233+GithubESPI@users.noreply.github.com>
2026-02-19 12:10:59 +0100

    changement du middleware

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,8 @@ import { NextResponse } from "next/server";
 export function middleware() {
   const response = NextResponse.next();
 
-  // ✅ CORS limité au domaine de l'application
-  // https://bulletin.groupe-espi.fr/
-  const allowedOrigin = process.env.NEXTAUTH_URL || "https://bulletin.groupe-espi.fr";
-
-  response.headers.set("Access-Control-Allow-Origin", allowedOrigin);
+  // Ajout des headers CORS
+  response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type, X-Auth-Token");
 
